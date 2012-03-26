22c513b3c0a6933ff3125e48f449ed8f12afff8d
max <max@ubuntu.localdomain>
2012-03-26 10:15:39 -0700

    fixup source formatting

--- src/FuseSource/Stomp/Exception/FrameException.php
+++ src/FuseSource/Stomp/Exception/FrameException.php
@@ -21,7 +21,7 @@ use FuseSource\Stomp\Value\Frame;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 
 class FrameException extends Exception implements ExceptionInterface
--- src/FuseSource/Stomp/Value/Uri.php
+++ src/FuseSource/Stomp/Value/Uri.php
@@ -19,49 +19,49 @@ use InvalidArgumentException;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 
 class Uri
 {
-    private $uriParts;
-    private $uriString;
-    
-    public function __construct($uriString)
-    {
-        $uriParts = @parse_url($uriString);
-        $this->assertValidUri($uriParts);
-        
-        $this->uriParts = $uriParts;
-        $this->uriString = $uriString;
-    }
-    
-    private function assertValidUri($uriParts)
-    {
-        if (!$uriParts) {
-            throw new InvalidArgumentException('Invalid broker URI');
-        }
-        
-        if (!isset($uriParts['scheme']) || $uriParts['scheme'] !== 'tcp') {
-            throw new InvalidArgumentException('Only tcp is supported as scheme for now');
-        }
-        
-        if (!isset($uriParts['port']) || !is_numeric($uriParts['port'])) {
-            throw new InvalidArgumentException('No valid port found');
-        }
-        
-        if (!isset($uriParts['host'])) {
-            throw new InvalidArgumentException('No host found');
-        }
-    }
-    
-    public function getPort()
-    {
-        return $this->uriParts['port'];
-    }
-    
-    public function getHost()
-    {
-        return $this->uriParts['host'];
-    }
+	private $uriParts;
+	private $uriString;
+
+	public function __construct($uriString)
+	{
+		$uriParts = @parse_url($uriString);
+		$this->assertValidUri($uriParts);
+
+		$this->uriParts = $uriParts;
+		$this->uriString = $uriString;
+	}
+
+	private function assertValidUri($uriParts)
+	{
+		if (!$uriParts) {
+			throw new InvalidArgumentException('Invalid broker URI');
+		}
+
+		if (!isset($uriParts['scheme']) || $uriParts['scheme'] !== 'tcp') {
+			throw new InvalidArgumentException('Only tcp is supported as scheme for now');
+		}
+
+		if (!isset($uriParts['port']) || !is_numeric($uriParts['port'])) {
+			throw new InvalidArgumentException('No valid port found');
+		}
+
+		if (!isset($uriParts['host'])) {
+			throw new InvalidArgumentException('No host found');
+		}
+	}
+
+	public function getPort()
+	{
+		return $this->uriParts['port'];
+	}
+
+	public function getHost()
+	{
+		return $this->uriParts['host'];
+	}
 }
